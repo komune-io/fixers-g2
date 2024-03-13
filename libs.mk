154ac997c995015509259ee9535a8d1508de9d4b
Adrien Poupard <1469248+apoupard@users.noreply.github.com>
2024-03-14 00:16:18 +0100

    feat(make-package): add reusable GitHub Actions workflow for running lint, build, test, and package commands using make feat(nodejs): add reusable GitHub Actions workflow for setting up Node.js environment and caching dependencies chore(docs.yml): update workflow to use the new make-nodejs-workflow.yml action chore(main.yml): update workflow to use the new make-nodejs-workflow.yml action and rename to "Libs Package" feat(make-nodejs-workflow.yml): create reusable GitHub Actions workflow for running make commands and uploading artifacts add libs.mk file with make commands for cleaning, linting, building, testing, and packaging the libraries update components package.json to use "publish:github" script instead of "publish:gitlab" The changes were made to improve the development workflow by introducing reusable GitHub Actions workflows for running make commands and uploading artifacts. The new workflows allow for easier configuration and execution of common tasks such as linting, building, testing, and packaging. The libs.mk file provides a set of make commands for managing the libraries, including cleaning, linting, building, testing, and packaging. The components package.json was also updated to use the "publish:github" script instead of "publish:gitlab" for publishing to GitHub Packages.
    chore(package.json): update publish scripts to publish to GitHub instead of GitLab
    The publish scripts in the package.json files of multiple packages have been updated to publish to GitHub instead of GitLab. This change was made to align with the project's migration from GitLab to GitHub for source code hosting and package publishing.

---
+++ libs.mk
@@ -0,0 +1,39 @@
+
+clean:
+	-rm -fr node_modules
+	-find ./packages/*/ -name "node_modules" -type d -exec rm -rf {} \;
+	-find ./packages/*/ -name "dist" -type d -exec rm -rf {} \;
+
+lint:
+	echo 'No Lint'
+
+build: build-libs
+
+test:
+	echo 'No Test'
+
+package: push-libs-github
+
+build-libs:
+	@yarn install --frozen-lockfile --ignore-scripts
+	@yarn workspace @komune-io/g2-utils run build
+	@yarn workspace @komune-io/g2-themes run build
+	@yarn workspace @komune-io/g2-notifications run build
+	@yarn workspace @komune-io/g2-components run build
+	@yarn workspace @komune-io/g2-forms run build
+	@yarn workspace @komune-io/g2-documentation run build
+	@yarn workspace @komune-io/g2-layout run build
+	@yarn workspace @komune-io/g2-composable run build
+	@yarn workspace @komune-io/g2-providers run build
+	@yarn workspace @komune-io/g2-s2 run build
+	@yarn workspace @komune-io/g2-im run build
+	@yarn workspace @komune-io/g2-fs run build
+	@yarn workspace @komune-io/g2 run build
+	@yarn workspace @komune-io/g2-storybook-documentation run build
+	@yarn workspace @komune-io/webpack-components run build
+
+push-libs-npm:
+	VERSION=${VERSION} yarn publishWorkspaces:npm
+
+push-libs-github:
+	TAG=${VERSION} yarn publishWorkspaces:github
