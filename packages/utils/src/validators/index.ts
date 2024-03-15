6aea064ea5258819717f98bc022a2c4615bff58b
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2024-03-15 17:10:16 +0100

    fix eslint config

--- packages/utils/src/validators/index.ts
+++ packages/utils/src/validators/index.ts
@@ -1,31 +1,31 @@
-import { TFunction } from "i18next";
+import { TFunction } from 'i18next'
 
-type PotentialError = string | undefined | Promise<string | undefined>;
-type ValidatorFnc = (value: any, values: any) => PotentialError;
-const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i;
+type PotentialError = string | undefined | Promise<string | undefined>
+type ValidatorFnc = (value: any, values: any) => PotentialError
+const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/i
 
 export const requiredFieldValidator = (message: string, value: any) => {
-  if (value == undefined || value.length === 0) return message;
-  const string = String(value).trim();
-  if (!string) return message;
-  return;
-};
+  if (value == undefined || value.length === 0) return message
+  const string = String(value).trim()
+  if (!string) return message
+  return
+}
 
 const requiredField =
   (t: TFunction): ValidatorFnc =>
   (value: any) => {
-    return requiredFieldValidator(t("g2.fieldRequired"), value);
-  };
+    return requiredFieldValidator(t('g2.fieldRequired'), value)
+  }
 
 const requiredFile =
   (t: TFunction, key: string, fileName: string): ValidatorFnc =>
   (value: any, values: any) => {
-    if (!!values[`${key}Uploaded`]) return undefined;
+    if (!!values[`${key}Uploaded`]) return undefined
     return requiredFieldValidator(
-      t("g2.fileRequired", { fileDesc: fileName }),
+      t('g2.fileRequired', { fileDesc: fileName }),
       value
-    );
-  };
+    )
+  }
 
 const nonNegativeNumberValidator = (
   requiredNumberMessage: string,
@@ -35,12 +35,12 @@ const nonNegativeNumberValidator = (
   required: boolean = true,
   zeroIncluded: boolean = false
 ) => {
-  if (value == undefined) return required ? requiredNumberMessage : undefined;
-  const number = typeof value === "number" ? value : Number(value);
-  if (zeroIncluded && number <= 0) return above0Message;
-  if (!zeroIncluded && number < 0) return positiveMessage;
-  return;
-};
+  if (value == undefined) return required ? requiredNumberMessage : undefined
+  const number = typeof value === 'number' ? value : Number(value)
+  if (zeroIncluded && number <= 0) return above0Message
+  if (!zeroIncluded && number < 0) return positiveMessage
+  return
+}
 
 const nonNegativeNumber =
   (
@@ -50,73 +50,73 @@ const nonNegativeNumber =
   ): ValidatorFnc =>
   (value: any) => {
     return nonNegativeNumberValidator(
-      t("g2.fieldRequired"),
-      t("g2.needFieldAbove0"),
-      t("g2.needFieldPositive"),
+      t('g2.fieldRequired'),
+      t('g2.needFieldAbove0'),
+      t('g2.needFieldPositive'),
       value,
       required,
       zeroIncluded
-    );
-  };
+    )
+  }
 
 export const phone = (t: TFunction) => (value?: string, values?: any) => {
-  const trimmed = (value ?? "").trim().replaceAll(" ", "");
+  const trimmed = (value ?? '').trim().replaceAll(' ', '')
   if (trimmed && (trimmed.length !== 10 || isNaN(Number(trimmed)))) {
-    return t("g2.phoneNeedsToHave10Digits");
+    return t('g2.phoneNeedsToHave10Digits')
   }
-  return requiredField(t)(value, values);
-};
+  return requiredField(t)(value, values)
+}
 
 export const email = (t: TFunction) => (value?: string, values?: any) => {
-  const error = requiredField(t)(value, values);
-  if (error) return error;
-  const trimmed = value!.trim();
-  if (!emailRegex.test(trimmed)) return t("g2.incorrectEmail");
-  return;
-};
+  const error = requiredField(t)(value, values)
+  if (error) return error
+  const trimmed = value!.trim()
+  if (!emailRegex.test(trimmed)) return t('g2.incorrectEmail')
+  return
+}
 
 const street = (t: TFunction) => (value?: string, values?: any) => {
-  const city = values.city?.trim();
-  const postalCode = values.postalCode?.trim();
-  const trimmed = value?.trim();
-  if ((!!postalCode || !!city) && !trimmed) return t("g2.requiredStreet");
-  return;
-};
+  const city = values.city?.trim()
+  const postalCode = values.postalCode?.trim()
+  const trimmed = value?.trim()
+  if ((!!postalCode || !!city) && !trimmed) return t('g2.requiredStreet')
+  return
+}
 
 const postalCode = (t: TFunction) => (value?: string, values?: any) => {
-  const street = !!values.street?.trim();
-  const city = !!values.city?.trim();
-  const trimmed = value?.trim();
-  if ((street || city) && !trimmed) return t("g2.requiredPostalCode");
-  return;
-};
+  const street = !!values.street?.trim()
+  const city = !!values.city?.trim()
+  const trimmed = value?.trim()
+  if ((street || city) && !trimmed) return t('g2.requiredPostalCode')
+  return
+}
 
 const city = (t: TFunction) => (value?: string, values?: any) => {
-  const street = values.street?.trim();
-  const postalCode = values.postalCode?.trim();
-  const trimmed = value?.trim();
-  if ((!!street || !!postalCode) && !trimmed) return t("g2.requiredCity");
-  return;
-};
+  const street = values.street?.trim()
+  const postalCode = values.postalCode?.trim()
+  const trimmed = value?.trim()
+  if ((!!street || !!postalCode) && !trimmed) return t('g2.requiredCity')
+  return
+}
 
 export const password = (t: TFunction) => (value?: string) => {
-  const password = value?.trim();
+  const password = value?.trim()
   if (!password) {
-    return t("g2.completeThePassword");
+    return t('g2.completeThePassword')
   }
   if (!!password && password?.length < 8) {
-    return t("g2.passWordLongerThan8");
+    return t('g2.passWordLongerThan8')
   }
-  return undefined;
-};
+  return undefined
+}
 
 export const passwordCheck =
   (t: TFunction) => (value?: string, values?: any) => {
-    const password = values.password?.trim();
-    const passwordCheck = value?.trim();
-    if (password !== passwordCheck) return t("g2.samePasswordCheck");
-    return undefined;
-  };
+    const password = values.password?.trim()
+    const passwordCheck = value?.trim()
+    if (password !== passwordCheck) return t('g2.samePasswordCheck')
+    return undefined
+  }
 
 export const validators = {
   requiredField,
@@ -128,5 +128,5 @@ export const validators = {
   postalCode,
   city,
   password,
-  passwordCheck,
-};
+  passwordCheck
+}
