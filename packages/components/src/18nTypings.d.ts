196da8bacb1363e7646328f2fe6c20208c67855b
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2024-03-21 09:43:50 +0100

    upgrade components package dependencies

--- packages/components/src/18nTypings.d.ts
+++ packages/components/src/18nTypings.d.ts
@@ -1,7 +1,17 @@
+// import the original type declarations
+// import the original type declarations
 import 'i18next'
+import { G2Translations } from '@komune-io/g2-providers'
 
 declare module 'i18next' {
+  // Extend CustomTypeOptions
   interface CustomTypeOptions {
+    // custom namespace type, if you changed it
+    defaultNS: 'translation'
+    // custom resources type
+    resources: {
+      g2: typeof G2Translations.fr
+    }
     returnNull: false
   }
 }
