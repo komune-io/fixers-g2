0f3b6d444b6e11f79ef771c2d4949de7cf4803ea
basile-savouret <47100280+basile-savouret@users.noreply.github.com>
2024-04-22 10:54:18 +0200

    fix builds

--- packages/components/src/Actions/ActionsWrapper.tsx
+++ packages/components/src/Actions/ActionsWrapper.tsx
@@ -6,7 +6,7 @@ import {
   ActionsStyles,
   Action
 } from './Actions'
-import { BasicProps } from '@komune-io/g2-themes/src'
+import { BasicProps } from '@komune-io/g2-themes'
 import { MergeMuiElementProps } from '@komune-io/g2-themes'
 
 export interface ActionWrapperClasses extends ActionsClasses {
