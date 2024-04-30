5293eec5209ca69913c36c70f778508231431f98
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-04-30 22:22:13 +0200

    feat(utils): add RequestProps export to http/index.ts The RequestProps module is now exported from http/index.ts, providing easier access to the RequestProps functionality for other modules within the http package.

--- packages/utils/src/http/index.ts
+++ packages/utils/src/http/index.ts
@@ -1,3 +1,4 @@
-export * from "./request";
-export * from "./useCommandRequest";
-export * from "./useQueryRequest";
+export * from './request'
+export * from './RequestProps'
+export * from './useCommandRequest'
+export * from './useQueryRequest'
