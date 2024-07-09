c67776b926d4c12600e903df78382e7677f83711
Adrien <email = 1469248+apoupard@users.noreply.github.com>
2024-07-09 14:17:54 +0200

    chore(useQueryRequest.ts): add console.log statements for debugging purposes The console.log statements were added to provide additional information for debugging purposes in the useQueryRequest function. This will help in tracking the flow of execution and identifying any potential issues during the query request process.

--- packages/utils/src/http/useQueryRequest.ts
+++ packages/utils/src/http/useQueryRequest.ts
@@ -1,51 +1,55 @@
 import {
   useQuery,
   useQueryClient,
-  UseQueryOptions,
-} from "@tanstack/react-query";
-import { useCallback } from "react";
-import { HttpOptions, errorHandler, request } from "./index";
-import { RequestProps } from "./RequestProps";
+  UseQueryOptions
+} from '@tanstack/react-query'
+import { useCallback } from 'react'
+import { HttpOptions, errorHandler, request } from './index'
+import { RequestProps } from './RequestProps'
 
 export type QueryOptions<QUERY, RESULT> = Omit<
   UseQueryOptions<RESULT | null, unknown, RESULT, [string, QUERY]>,
-  "queryKey" | "queryFn"
->;
+  'queryKey' | 'queryFn'
+>
 
 export type QueryParams<QUERY, RESULT> = {
-  queryKey?: string;
-  options?: QueryOptions<QUERY, RESULT>;
-  query: QUERY;
-};
+  queryKey?: string
+  options?: QueryOptions<QUERY, RESULT>
+  query: QUERY
+}
 
 export const useQueryRequest = <QUERY, RESULT>(
   path: string,
   props: RequestProps,
-  params: QueryParams<QUERY, RESULT>,
+  params: QueryParams<QUERY, RESULT>
 ) => {
-  const { queryKey = path, options } = params;
-  const queryFn = useFetchQueryRequest<QUERY, RESULT>(path, props);
-  const queryClient = useQueryClient();
+  const { queryKey = path, options } = params
+  console.log('useQueryRequest: 1')
+  const queryFn = useFetchQueryRequest<QUERY, RESULT>(path, props)
+  console.log('useQueryRequest: 2')
+  const queryClient = useQueryClient()
+  console.log('useQueryRequest: 3')
 
-  const invalidateQuery = useCallback(
-    () => queryClient.invalidateQueries({ queryKey: [queryKey] }),
-    [queryClient.invalidateQueries, queryKey],
-  );
+  const invalidateQuery = useCallback(() => {
+    console.log('useQueryRequest.invalidateQuery: 1')
+    return queryClient.invalidateQueries({ queryKey: [queryKey] })
+  }, [queryClient.invalidateQueries, queryKey])
+  console.log('useQueryRequest: 4')
 
   return {
     ...useQuery<RESULT | null, unknown, RESULT, [string, QUERY]>({
       queryKey: [queryKey, params.query],
       queryFn: () => queryFn(params.query),
-      ...options,
+      ...options
     }),
     key: queryKey,
-    invalidateQuery: invalidateQuery,
-  };
-};
+    invalidateQuery: invalidateQuery
+  }
+}
 
 export const useFetchQueryRequest = <QUERY, RESULT>(
   path: string,
-  props: RequestProps,
+  props: RequestProps
 ): ((query?: QUERY) => Promise<RESULT | null>) => {
   return useCallback(
     async (query?: QUERY) =>
@@ -54,41 +58,41 @@ export const useFetchQueryRequest = <QUERY, RESULT>(
             await fetchQueryRequest<QUERY, RESULT[]>(path, query, props)
           )?.pop() ?? null
         : null,
-    [props],
-  );
-};
+    [props]
+  )
+}
 
 export const useFetchBinary = <QUERY>(
   path: string,
-  props: RequestProps,
+  props: RequestProps
 ): ((query?: QUERY) => Promise<string | undefined>) => {
   return useCallback(
     async (query?: QUERY) =>
       query &&
-      fetchQueryRequest(path, query, props, { returnType: "objectUrl" }),
-    [props],
-  );
-};
+      fetchQueryRequest(path, query, props, { returnType: 'objectUrl' }),
+    [props]
+  )
+}
 
 export const fetchQueryRequest = async <QUERY, RESULT>(
   path: string,
   query: QUERY,
   props: RequestProps,
-  options?: Partial<HttpOptions>,
+  options?: Partial<HttpOptions>
 ) => {
-  const { jwt, url } = props;
+  const { jwt, url } = props
   const res = await request<RESULT>({
     url: `${url}/${path}`,
-    method: "POST",
+    method: 'POST',
     body: JSON.stringify(query),
     jwt: jwt,
     errorHandler: errorHandler(path),
-    ...options,
-  });
+    ...options
+  })
   if (res) {
     //no success handler on a query
-    return res;
+    return res
   } else {
-    return undefined;
+    return undefined
   }
-};
+}
